b04e041495ddfad1addd77988fef7115af490097
Kuznetsov Igor <igorkuzentsov14@gmail.com>
2019-04-02 03:08:35 +0300

    Basic hooks support added

--- scripts/train/train_setup.py
+++ scripts/train/train_setup.py
@@ -33,8 +33,6 @@ class Prototypical(Model):
         )
 
     def call(self, support, query):
-        print("Support shape: ", support.shape)
-
         n_class = support.shape[0]
         n_support = support.shape[1]
         n_query = query.shape[1]
@@ -42,48 +40,33 @@ class Prototypical(Model):
         h = support.shape[3]
         c = support.shape[4]
 
-        print("n_class: ", n_class)
-
         target_inds = tf.reshape(tf.range(n_class), [n_class, 1])
-        print("Target inds original: ", target_inds.shape)
         target_inds = tf.tile(target_inds, [1, n_support])
-        print("Target inds: ", target_inds.shape)
-
-        print(support.shape, query.shape)
 
         cat = tf.concat([
             tf.reshape(support, [n_class * n_support, w, h, c]),
             tf.reshape(query, [n_class * n_query, w, h, c])], axis=0)
-        print("cat: ", cat.shape)
 
         z = self.encoder(cat)
-        print("z: ", z.shape)
         z_prototypes = tf.reshape(z[:n_class * n_support],
                                   [n_class, n_support, z.shape[-1]])
         z_prototypes = tf.math.reduce_mean(z_prototypes, axis=1)
-        print("z_prototypes: ", z_prototypes.shape)
-
         z_query = z[n_class * n_support:]
-        print("z_query: ", z_query.shape)
 
         dists = calc_euclidian_dists(z_query, z_prototypes)
 
         log_p_y = tf.nn.log_softmax(-dists, axis=-1)
         log_p_y = tf.reshape(log_p_y, [n_class, n_query, -1])
-        print("Log Prob: ", log_p_y.shape)
 
         inds = [[i, i//n_support] for i in list(range(n_class * n_query))]
-        print("INDS: ", len(inds), len(inds[0]))
 
         loss = -tf.gather_nd(tf.reshape(log_p_y, [n_class*n_query, -1]), inds)
         loss = tf.reshape(loss, [n_class, n_query])
-        print("loss: ", loss.shape)
         loss_mean = tf.reduce_mean(loss)
 
         y_pred = tf.math.argmax(log_p_y, axis=2)
         eq = tf.math.equal(tf.cast(y_pred, tf.int32), tf.cast(target_inds, tf.int32))
         eq = tf.cast(eq, np.int32)
-        print("eq: ", eq.shape)
         acc = tf.reduce_sum(eq) / (n_class * n_query)
 
         return loss_mean, acc
@@ -100,6 +83,13 @@ def calc_euclidian_dists(x, y):
 class TrainEngine(object):
     def __init__(self, config):
         self.config = config
+        self.hooks = {name: lambda state: None
+                      for name in ['on_start',
+                                   'on_start_epoch',
+                                   'on_end_epoch',
+                                   'on_start_episode',
+                                   'on_end_episode',
+                                   'on_end']}
 
     def loss(self, support, query):
         loss, acc = self.model(support, query)
@@ -107,12 +97,18 @@ class TrainEngine(object):
 
     def train(self, train_loader, val_loader, **kwargs):
         self.model = Prototypical()
+        optimizer = tf.keras.optimizers.Adam(self.config['train.lr'])
 
         train_loss = tf.metrics.Mean(name='train_loss')
         val_loss = tf.metrics.Mean(name='val_loss')
         train_acc = tf.metrics.Mean(name='train_accuracy')
         val_acc = tf.metrics.Mean(name='val_accuracy')
-        optimizer = tf.keras.optimizers.Adam(self.config['train.lr'])
+
+        state = {
+            'model': self.model,
+            'epoch': 1,
+            'total_episode': 1,
+        }
 
         @tf.function
         def train_step(support, query):
@@ -135,11 +131,16 @@ class TrainEngine(object):
 
         n_episodes = self.config['data.train_episodes']
         n_epochs = self.config['train.epochs']
+        self.hooks['on_start'](state)
         for epoch in range(n_epochs):
+            self.hooks['on_start_epoch'](state)
             for i_episode, (support, query) in enumerate(train_loader):
+                self.hooks['on_start_episode'](state)
                 train_step(support, query)
                 if i_episode+1 == n_episodes:
                     break
+                self.hooks['on_end_episode'](state)
+                state['total_episode'] += 1
             for support, query in val_loader:
                 val_step(support, query)
 
@@ -147,7 +148,10 @@ class TrainEngine(object):
             print(template.format(epoch + 1, train_loss.result(), train_acc.result(),
                                   val_loss.result(),
                                   val_acc.result() * 100))
+            self.hooks['on_end_episode'](train)
+            state['epoch'] += 1
 
+        self.hooks['on_end'](state)
         print("Success!")
 
 
@@ -165,9 +169,35 @@ def train(config):
     else:
         device_name = 'CPU:0'
 
-    experiment = TrainEngine(config)
+    train_engine = TrainEngine(config)
+
+    # Set hooks on training process
+    def on_start(state):
+        print("Training started.")
+    train_engine.hooks['on_start'] = on_start
+
+    def on_end(state):
+        print("Training ended.")
+    train_engine.hooks['on_end'] = on_end
+
+    def on_start_epoch(state):
+        print(f"Epoch {state['epoch']} started.")
+    train_engine.hooks['on_start_epoch'] = on_start_epoch
+
+    def on_end_epoch(state):
+        print(f"Epoch {state['epoch']} ended.")
+    train_engine.hooks['on_end_epoch'] = on_end_epoch
+
+    def on_start_episode(state):
+        print(f"Episode {state['total_episode']}")
+    train_engine.hooks['on_start_episode'] = on_start_episode
+
+    def on_end_episode(state):
+        return None
+    train_engine.hooks['on_end_episode'] = on_end_episode
+
     with tf.device(device_name):
-        experiment.train(
+        train_engine.train(
             model=None,
             train_loader=train_loader,
             val_loader=val_loader,
